6ce675965f315ec9d2f3d10a1a6fa26683ae67a0
Enzo Peralta <Enzo.Peralta@odfjell.com>
2022-04-05 15:03:00 +0800

    Update files

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from appname import create_app
-#from tracker import db
 
 app = create_app()
-
-#with app.app_context():
-#   db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
